fc5606ef5a95cee3bd359b93ae367c4ade31a482
Jinwei Chang <cwchang93@gmail.com>
2018-08-08 15:20:11 +0800

    1st version to github

--- functional_tests.py
+++ functional_tests.py
@@ -1,14 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
+
+
     def tearDown(self):
         self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://hocalhost:8000')
@@ -19,15 +27,21 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.Enter)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text =='1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect,render
 from django.http import HttpResponse
+from lists.models import Item
 # Create your views here.
 #home_page = None
 def home_page(request):
+    if request.mothod == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
     return render(request, 'home.html')
